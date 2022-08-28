0e2a2a039fa770b00d85d23f80c1d1bbee3ca572
muhyasin89 <muhyasin89@gmail.com>
2022-08-28 07:07:02 +0700

    error modelBuilder.UseIdentityColumns Add project files.

---
+++ IssueApp/Issue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueApp
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
---
+++ IssueApp/IssueDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace IssueApp
+{
+    public class IssueDataContext: DbContext
+    {
+        public IssueDataContext(DbContextOptions<IssueDataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseIdentityColumns();
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
