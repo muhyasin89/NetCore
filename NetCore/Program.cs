76c8c7282c0ec516edd4d33d80fa747af985ee69
muhyasin89 <muhyasin89@gmail.com>
2022-10-12 21:29:48 +0700

    remove merge conflict

--- NetCore/Program.cs
+++ NetCore/Program.cs
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+
 using System.Configuration;
 using IssueApp;
 using Microsoft.EntityFrameworkCore;
 
-=======
->>>>>>> 4979ece31bb39fb0468cd0f993a40d1017685f6e
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,12 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-<<<<<<< HEAD
+
 builder.Services.AddDbContext<IssueDataContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
-=======
->>>>>>> 4979ece31bb39fb0468cd0f993a40d1017685f6e
+
 
 var app = builder.Build();
 
