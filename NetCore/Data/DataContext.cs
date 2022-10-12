af1c6e878079b828a26e706bcb66570650397dde
muhyasin89 <muhyasin89@gmail.com>
2022-10-12 22:30:22 +0700

    first Init

---
+++ NetCore/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore.Models;
+
+namespace NetCore.Data
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ NetCore/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NetCore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        public DateTime DOB { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
