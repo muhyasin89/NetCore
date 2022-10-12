9310eb4c2fe354e87acfbf33025eae4e53e077d2
muhyasin89 <muhyasin89@gmail.com>
2022-10-12 21:50:12 +0700

    Add Issue Db

--- NetCore/Program.cs
+++ NetCore/Program.cs
@@ -1,3 +1,5 @@
+using NetCore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<IssueDbContext>(
+    o=>o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
 
 var app = builder.Build();
 
